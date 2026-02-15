09bed6367e6603a3e1c2812444b64ee33bb313fd
zerocuse <zmgrande@syr.edu>
2026-02-15 14:06:25 -0500

    Added navigation to account page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
 import { useState } from 'react'
 
 import './App.css'
+import Navbar from "./Navbar"
+import Account from "./Account" // Import your Account component
 import Search from "./search/search"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    
-    <Search />
-    </>
+    <Router>
+      <Navbar />
+      
+      <Routes>
+        <Route path="/" element={
+          <>
+            <h1 className="cited">Cited.</h1>
+            <p>Your personal fact checker.</p>
+            <Search />
+          </>
+        } />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </Router>
   )
 }
 

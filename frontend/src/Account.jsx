cbbb6758a8bfadf544393f6bb6ebc50827ef17cb
zerocuse <zmgrande@syr.edu>
2026-02-15 14:42:10 -0500

    More Navigation Steps

--- frontend/src/Account.jsx
+++ frontend/src/Account.jsx
@@ -3,12 +3,10 @@ import { useState } from 'react'
 import './Account.css'
 
 function Account() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <h1 className="account">Account</h1>
-    <link rel="stylesheet" href="App.jsx" />
     </>
   )
 }

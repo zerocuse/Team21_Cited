169fcc003d30321866f91e0494cda664d87f56fe
zerocuse <zmgrande@syr.edu>
2026-02-15 17:25:02 -0500

    Initial Homepage Implementation (Slightly Functional)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-
 import './App.css'
 import Navbar from "./Navbar"
-import Account from "./Account" // Import your Account component
-import Search from "./search/search"
+import Account from "./Account"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Settings from './Settings'
 import SearchUpdated from './SearchUpdated/SearchUpdated'
@@ -11,21 +9,23 @@ import SearchUpdated from './SearchUpdated/SearchUpdated'
 function App() {
   return (
     <Router>
-
-      <SearchUpdated />
-
-      <Navbar />
-      
-      <Routes>
-        <Route path="/" element={
-          <>
-            <h1 className="cited">Cited.</h1>
-            <p>Your personal fact checker.</p>
-          </>
-        } />
-        <Route path="/account" element={<Account />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
+      <div className="app-container">
+        <Navbar />
+        
+        <Routes>
+          <Route path="/" element={
+            <div className="hero-section">
+              <div className="hero">
+                <h1 className="cited">Cited.</h1>
+                <p>Your personal fact checker.</p>
+              </div>
+              <SearchUpdated />
+            </div>
+          } />
+          <Route path="/account" element={<Account />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
     </Router>
   )
 }
--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -5,9 +5,13 @@ import "./Navbar.css"
 function Navbar() {
     return (
         <header className="cited-nav">
-            <Link to="/"><img src="./src/assets/home_icon.svg" alt="home" /></Link>
-            <Link to="/account"><img src="./src/assets/account_icon.svg" alt="account" /></Link>
-            <Link to="/settings"><img src="./src/assets/settings_icon.svg" alt="settings" /></Link>
+            <div className="nav-left">
+                <Link to="/"><img src="./src/assets/home_icon.svg" alt="home" /></Link>
+            </div>
+            <div className="nav-right">
+                <Link to="/account"><img src="./src/assets/account_icon.svg" alt="account"/></Link>
+                <Link to="/settings"><img src="./src/assets/settings_icon.svg" alt="settings" className="settings-icon" /></Link>
+            </div>
         </header>
     )
 }

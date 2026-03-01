97126b24daa43367113a20fee61175a15025d596
Adam Knell <adamandasa@gmail.com>
2026-03-01 14:06:43 -0500

    added more funtions in user service and merged back to main branch

--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -30,4 +30,32 @@ def create_user(username: str, email_address: str, membership_status: str, first
     db.session.add(new_user)
     db.session.commit()
     return new_user
+
+def get_user_by_id(userID: int) -> User:
+    user = User.query.get(userID)
+    if not user:
+        raise ValueError(f"User with ID {userID} does not exist")
+    return user
+
+def get_user_by_username(username: str) -> User:
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        raise ValueError(f"User with username '{username}' does not exist")
+    return user
+
+def get_user_by_email(email_address: str) -> User:
+    user = User.query.filter_by(email_address=email_address).first()
+    if not user:
+        raise ValueError(f"User with email address '{email_address}' does not exist")
+    return user
+
+def delete_user(userID: int) -> None:
+    user = User.query.get(userID)
+    if not user:
+        raise ValueError(f"User with ID {userID} does not exist")
+    
+    db.session.delete(user)
+    db.session.commit()
+
+
     

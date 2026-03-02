125b32e88153d13701013057b90f73f7ec9797ad
zerocuse <zmgrande@syr.edu>
2026-03-02 16:11:43 -0500

    Searchbar Updates

--- frontend/src/SearchUpdated/SearchUpdated.jsx
+++ frontend/src/SearchUpdated/SearchUpdated.jsx
@@ -105,17 +105,7 @@ function SearchUpdated() {
 
 				<div className="search-actions">
 					<label htmlFor="file-upload" className="icon-button">
-						<svg
-							width="20"
-							height="20"
-							viewBox="0 0 24 24"
-							fill="none"
-							stroke="currentColor"
-							strokeWidth="2">
-							<rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
-							<circle cx="8.5" cy="8.5" r="1.5" />
-							<polyline points="21 15 16 10 5 21" />
-						</svg>
+						<img src="./src/assets/attachment-icon.svg" alt="fileupload" className='attachment-icon'/>
 						<input
 							id="file-upload"
 							type="file"
